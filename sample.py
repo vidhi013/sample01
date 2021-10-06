956e1a980fe968c284288166918ef365c3e14c33
vidhi013 <vidhi.r.kashyap@gmail.com>
2021-10-06 14:23:25 +0530

    first commit

---
+++ sample.py
@@ -0,0 +1,10 @@
+#largetest of 3
+num1=int(input("enter the num1"))
+num2=int(input("enter the num2"))
+num3=int(input("enter the num3"))
+if num1>num2 and num1>num3:
+    print("num1 is greater")
+elif num2>num3:
+    print("num2 is greater")
+else:
+    print("num3 is greater")
